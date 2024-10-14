73c7e35c4943c7539c245a82231fc4973b6ad5d5
FatherofElias <supra_3_8@hotmail.com>
2024-10-14 18:23:43 -0500

    update

---
+++ stringManipulation.js
@@ -0,0 +1,7 @@
+// Assignment 2
+// Task 1
+let message = "Hello, World!";
+
+// Using the length property to get the length of the string
+let lengthOfString = message.length;
+console.log(`The length of the message is: ${lengthOfString}`);
