eb8579d88f2440978ea9d7afb49a77a4f9c187a5
FatherofElias <supra_3_8@hotmail.com>
2024-10-14 18:25:50 -0500

    update

--- stringManipulation.js
+++ stringManipulation.js
@@ -5,3 +5,15 @@ let message = "Hello, World!";
 // Using the length property to get the length of the string
 let lengthOfString = message.length;
 console.log(`The length of the message is: ${lengthOfString}`);
+
+// Task 2
+let text = "Hello, World!";
+
+// Converting to uppercase
+let upperCaseText = text.toUpperCase();
+console.log(upperCaseText);
+
+// Converting to lowercase
+let lowerCaseText = text.toLowerCase();
+console.log(lowerCaseText); 
+
