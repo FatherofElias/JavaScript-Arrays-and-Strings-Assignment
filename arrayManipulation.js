cf24db47eb91f38283f3862ef3e4706a6334cd83
FatherofElias <supra_3_8@hotmail.com>
2024-10-14 18:20:11 -0500

    update

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -7,8 +7,8 @@ console.log(fruits); // Output: ['apple', 'banana', 'orange', 'grape']
 
 // Removing the last element
 let removedFruit = fruits.pop();
-console.log(fruits);        // Output: ['apple', 'banana', 'orange']
-console.log(removedFruit);  // Output: 'grape'
+console.log(fruits);        
+console.log(removedFruit);  
 
 
 // Task 2
@@ -17,4 +17,20 @@ let numbers = [3, 1, 5, 2, 4];
 // Sorting the array in ascending order
 numbers.sort((a, b) => a - b);
 
-console.log(numbers); // Output: [1, 2, 3, 4, 5]
+console.log(numbers); 
+
+
+// Task 3
+// let numbers = [3, 1, 5, 2, 4]; // This is the same as previous task so i quoted it out so no error would occur.
+
+// Using map to double each number
+let doubled = numbers.map(num => num * 2);
+console.log(doubled); 
+
+// Using filter to get even numbers
+let evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers); 
+
+// Using reduce to calculate the sum
+let sum = numbers.reduce((acc, num) => acc + num, 0);
+console.log(sum); 
